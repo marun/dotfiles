6a3c2f240fd957061add598cc3c9cf090db7ff3d
Maru Newby <marun@redhat.com>
2019-06-02 14:29:21 +0300

    Update apply.sh to work with macos

--- apply.sh
+++ apply.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -o errexit
 set -o nounset
@@ -11,6 +11,13 @@ stow --dir="${SRC_DIR}" --target="${HOME}" screen
 stow --dir="${SRC_DIR}" --target="${HOME}" vim
 stow --dir="${SRC_DIR}" --target="${HOME}" bash
 
-if ! grep -q '.bash_custom' ~/.bashrc > /dev/null; then
-  echo 'if [ -f ~/.bash_custom ]; then . ~/.bash_custom; fi' >> ~/.bashrc
+# Linux
+BASH_PROFILE=".bashrc"
+# Mac
+if which sw_vers &> /dev/null; then
+  BASH_PROFILE=".bash_profile"
+fi
+
+if ! grep -q '.bash_custom' ~/${BASH_PROFILE} > /dev/null; then
+  echo 'if [ -f ~/.bash_custom ]; then . ~/.bash_custom; fi' >> ~/${BASH_PROFILE}
 fi

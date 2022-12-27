bed1b70bcbcec129df0931bc6639335f3f6f4036
Maru Newby <me@maru.ca>
2022-12-27 13:52:52 -0800

    WIP

--- apply.sh
+++ apply.sh
@@ -10,15 +10,17 @@ stow --dir="${SRC_DIR}" --target="${HOME}" aliases
 stow --dir="${SRC_DIR}" --target="${HOME}" git
 stow --dir="${SRC_DIR}" --target="${HOME}" screen
 stow --dir="${SRC_DIR}" --target="${HOME}" vim
-stow --dir="${SRC_DIR}" --target="${HOME}" bash
 
-# Linux
-BASH_PROFILE=".bashrc"
 # Mac
 if which sw_vers &> /dev/null; then
-  BASH_PROFILE=".bash_profile"
-fi
-
-if ! grep -q '.bash_custom' ~/${BASH_PROFILE} > /dev/null; then
-  echo 'if [ -f ~/.bash_custom ]; then . ~/.bash_custom; fi' >> ~/${BASH_PROFILE}
+  stow --dir="${SRC_DIR}" --target="${HOME}" zsh
+  if ! grep -q '.zsh_custom' ~/.zshrc > /dev/null; then
+    echo 'if [ -f ~/.zsh_custom ]; then . ~/.zsh_custom; fi' >> ~/.zshrc
+  fi
+# Linux
+else
+  stow --dir="${SRC_DIR}" --target="${HOME}" bash
+  if ! grep -q '.bash_custom' ~/.bashrc > /dev/null; then
+    echo 'if [ -f ~/.bash_custom ]; then . ~/.bash_custom; fi' >> ~/.bashrc
+  fi
 fi
--- remove.sh
+++ remove.sh
@@ -10,6 +10,13 @@ stow -D --dir="${SRC_DIR}" --target="${HOME}" aliases
 stow -D --dir="${SRC_DIR}" --target="${HOME}" git
 stow -D --dir="${SRC_DIR}" --target="${HOME}" screen
 stow -D --dir="${SRC_DIR}" --target="${HOME}" vim
-stow -D --dir="${SRC_DIR}" --target="${HOME}" bash
 
-sed -i -e '/^.*bash_custom.*$/d' ~/.bashrc
+# Mac
+if which sw_vers &> /dev/null; then
+  stow -D --dir="${SRC_DIR}" --target="${HOME}" zsh
+  sed -i -e '/^.*zsh_custom.*$/d' ~/.zshrc
+# Linux
+else
+  stow -D --dir="${SRC_DIR}" --target="${HOME}" bash
+  sed -i -e '/^.*bash_custom.*$/d' ~/.bashrc
+fi
